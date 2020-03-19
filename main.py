e9878347dc150010b30e41c413cb0a192b70be29
dekoderer@gmail.com <dekoderer@gmail.com>
2020-03-19 08:42:54 +0100

    Added timestamp to screens.

--- main.py
+++ main.py
@@ -1,67 +1,70 @@
-from selenium import webdriver
-from pathlib import Path
 import time
-from datetime import datetime
-
+from pathlib import Path
+from selenium import webdriver
 from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException
 
 CORONA_VIRUS_URL = 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html' \
                    '#/bda7594740fd40299423467b48e9ecf6'
 FLIGHT_RADAR_URL = 'https://www.flightradar24.com/33.28,-21.42/3'
 CORONA_DECREMENT_BUTTON = 'esriSimpleSliderDecrementButton'
-CORONA_VIRUS_IMAGE = 'corona_virus_shots/corona_virus{}.png'
-FLIGHT_RADAR_IMAGE = 'flight_radar_shots/flight_radar{}.png'
 CORONA_VIRUS_DIR = 'corona_virus_shots'
 FLIGHT_RADAR_DIR = 'flight_radar_shots'
+CORONA_VIRUS_IMAGE = CORONA_VIRUS_DIR + '/corona_virus-{}.png'
+FLIGHT_RADAR_IMAGE = FLIGHT_RADAR_DIR + '/flight_radar-{}.png'
 FLIGHT_CLOSE_BUTTON = 'btn.btn-blue'
 REFRESH_TIME = 8
-SLEEP_TIME_AFTER_CLICKING_THE_BUTTON = 8
+NEXT_SCREENSHOT_DURATION = 3600
 
 
 def run():
     create_dir_if_not_exists(CORONA_VIRUS_DIR)
     create_dir_if_not_exists(FLIGHT_RADAR_DIR)
-    counter = 0
     while True:
         driver = webdriver.Chrome()
         driver.set_window_size(2560, 1440)
         driver.fullscreen_window()
-        save_screen_shot(CORONA_VIRUS_URL, counter, driver, CORONA_VIRUS_IMAGE)
-        save_screen_shot(FLIGHT_RADAR_URL, counter, driver, FLIGHT_RADAR_IMAGE)
-        counter += 1
+        save_screen_shot(CORONA_VIRUS_URL, driver, CORONA_VIRUS_IMAGE)
+        save_screen_shot(FLIGHT_RADAR_URL, driver, FLIGHT_RADAR_IMAGE)
         driver.close()
+        time.sleep(NEXT_SCREENSHOT_DURATION)
 
 
 def create_dir_if_not_exists(directory):
     try:
         Path(directory).mkdir(exist_ok=True)
     except FileNotFoundError:
-        now = datetime.now()
-        timestamp = datetime.timestamp(now)
-        print('{} Not possible to create directory {}'.format(timestamp, directory))
+        print('{} Not possible to create directory {}'.format(get_timestamp(), directory))
 
 
-def save_screen_shot(url, counter, driver, output_dir):
+def get_timestamp():
+    t = time.localtime()
+    current_time = time.strftime("%d_%m_%Y-%H_%M_%S", t)
+    return current_time
+
+
+def save_screen_shot(url, driver, image_name):
     driver.get(url)
     driver.refresh()
     time.sleep(REFRESH_TIME)
     if url in CORONA_VIRUS_URL:
-        click_button(driver, CORONA_DECREMENT_BUTTON, counter)
+        click_button(driver, CORONA_DECREMENT_BUTTON)
     if url in FLIGHT_RADAR_URL:
-        click_button(driver, FLIGHT_CLOSE_BUTTON, counter)
-    driver.save_screenshot(output_dir.format(counter))
+        click_button(driver, FLIGHT_CLOSE_BUTTON)
+    current_image_name = image_name.format(get_timestamp())
+    driver.save_screenshot(current_image_name)
+    print('{} screenshot {}'.format(get_timestamp(), current_image_name))
 
 
-def click_button(driver, button_to_click, counter):
+def click_button(driver, button_to_click):
     try:
         current_button = driver.find_element_by_class_name(button_to_click)
         current_button.click()
-        time.sleep(SLEEP_TIME_AFTER_CLICKING_THE_BUTTON)
+        time.sleep(REFRESH_TIME)
     except (NoSuchElementException, ElementNotInteractableException) as ex:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print('{} No such {} element found [counter={}], {}'.format(current_time, button_to_click, counter, ex))
+        print('{} No such {} element found {}'.format(get_timestamp(), button_to_click, ex))
 
 
 if __name__ == '__main__':
+    print('{} Starting screenshotter...'.format(get_timestamp()))
     run()
+    print('{} Stopping screenshotter...'.format(get_timestamp()))
